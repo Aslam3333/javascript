e39fbf8c66cc23431e0b695298f12c2111042f51
Aslam3333 <mohammedaslam3912@gmail.com>
2024-11-06 09:30:58 +0530

    grade

--- grade.js
+++ grade.js
@@ -1,18 +1,75 @@
-function evaluateGrade(score) {
-    if (score >= 90 && score <= 100) {
-        return "Grade A";
-    } else if (score >= 80 && score < 90) {
-        return "Grade B";
-    } else if (score >= 70 && score < 80) {
-        return "Grade C";
-    } else if (score >= 60 && score < 70) {
-        return "Grade D";
-    } else if (score >= 50 && score < 60) {
-        return "Grade E";
-    } else if (score < 50 && score >= 0) {
-        return "Grade F";
-    } else {
-        return "Invalid score. Please enter a number between 0 and 100.";
-    }
+// let score=67;
+
+//     if (score >= 90 && score <= 100) {
+//         console.log("grade A");
+        
+//     } else if (score >= 80 && score < 90) {
+//         console.log("grade B");
+//     } else if (score >= 70 && score < 80) {
+//         console.log("grade C");
+//     } else if (score >= 60 && score < 70) {
+//         console.log("grade D");
+//     } else if (score >= 50 && score < 60) {
+//         console.log("grade E");
+//     } else if (score < 50 && score >= 0) {
+//         console.log("grade F");
+//     } else {
+//         console.log("grade G");    }
+//         //SCORE
+// let age=90;
+// if (age >= 0 && age <= 12){
+//     console.log("child");
+   
+// }
+// else if (age >= 13 && age <= 19){
+//     console.log("teenage");
+    
+// }
+// else if (age >= 20 && age <= 35){
+//     console.log("young adult");
+    
+// }
+// else if (age >= 36 && age <= 55){
+//     console.log("adult");
+    
+// }
+// else if (age >= 55 && age <= 120){
+//     console.log("senior");
+// }
+// else {
+//     console.log("adult");
+// }
+// // age
+// let age=100;
+// switch (true) {
+//     case (age >= 0 && age <= 12):
+//         console.log("child");
+//         break;
+//     case (age >= 13 && age <= 19):
+//         console.log("teenage");
+//         break;
+//     case (age >= 20 && age <= 35):
+//         console.log("young adult");
+//         break;
+//     case (age >= 36 && age <= 55):
+//         console.log("adult");
+//         break;
+//     case (age >= 56 && age <= 120):
+//         console.log("senior");
+//         break;
+//     default:
+//         console.log("Invalid age");
+// }
+// //switch
+
+let car=["bmw","audi","ferrari","porshe"];
+let length=car.length;
+let text="";
+for (let i = 0;i < car.length;i++) {
+   text+=car[i];
+    
 }
-let 
+console.log(text);
+
+
+
